f527428091f13c1da702f818cee9168eef2bc1e1
mhceg791-199 <mhceg791@gmail.com>
2026-03-09 12:48:46 +0200

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: "../../var/www/mosaic-engineering", 
+    outDir: "dist"
   },
   plugins: [react()],
 })
