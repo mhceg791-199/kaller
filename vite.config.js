f394a49692b614154991239d1fec44406724d2f0
mhceg791-199 <mhceg791@gmail.com>
2026-03-19 13:07:36 +0200

    upload vite config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   build: {
-//     outDir: "dist"
-//   },
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: "../../var/www/kaller", 
+    outDir: "dist"
   },
   plugins: [react()],
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   build: {
+//     outDir: "../../var/www/kaller", 
+//   },
+//   plugins: [react()],
+// })
